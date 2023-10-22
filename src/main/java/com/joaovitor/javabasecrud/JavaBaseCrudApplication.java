9c4d65effc024c399a9746e3fec0b69d26bb5983
Joao Vitor Muniz Lopes <joao.lopes@mundiale.com.br>
2023-10-22 20:42:12 -0300

    first commit

---
+++ src/main/java/com/joaovitor/javabasecrud/JavaBaseCrudApplication.java
@@ -0,0 +1,13 @@
+package com.joaovitor.javabasecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBaseCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBaseCrudApplication.class, args);
+	}
+
+}
